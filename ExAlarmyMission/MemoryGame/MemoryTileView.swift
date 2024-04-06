9492dd17126c99f8a5b2aac37d619a8b07538a28
kangddong <rkdehddud96@naver.com>
2024-04-06 21:25:51 +0900

    [Feat] make flip tile view

--- ExAlarmyMission/MemoryGame/MemoryTileView.swift
+++ ExAlarmyMission/MemoryGame/MemoryTileView.swift
@@ -12,12 +12,29 @@ protocol MemoryTileViewAble {
     func flip()
 }
 
-final class MemoryTileView: UIView {
+final class MemoryTileView: UIView, MemoryTileViewAble {
+    let correctColor: UIColor = .systemOrange
+    let defaultColor: UIColor = .darkGray
+    
     init() {
         super.init(frame: .zero)
+        backgroundColor = defaultColor
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tappedView)))
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func tappedView() {
+        flip()
+    }
+    
+    func flip() {
+        let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
+        
+        UIView.transition(with: self, duration: 1.0, options: transitionOptions, animations: {
+            self.backgroundColor = self.correctColor
+        })
     }
 }
